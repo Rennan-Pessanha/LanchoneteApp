2c4a8dba6e168ccabc884721916e978f4e94b343
Rennan Pessanha <94031223+Rennan-Pessanha@users.noreply.github.com>
2024-05-27 23:13:34 -0300

    Att

--- src/pages/Card/Card.js
+++ src/pages/Card/Card.js
@@ -1,67 +1,119 @@
-import React from 'react';
-import { View, Text, StyleSheet, Image, ScrollView } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, StyleSheet, Image, ScrollView, TouchableOpacity, Modal } from 'react-native';
+
+// Dados das comidas!! ( Só ligar ao back galera!! )
+const categorias = [
+  {
+    id: 1,
+    nome: 'Burguers',
+    comidas: [
+      {
+        id: 1,
+        nome: 'Combo fritas e refri',
+        imagem: require('../../../assets/Lanche1.jpeg'),
+      },
+      {
+        id: 2,
+        nome: 'Hambuerguer',
+        imagem: require('../../../assets/Lanche2.jpeg'),
+      },
+    ],
+  },
+  {
+    id: 2,
+    nome: 'Fritas',
+    comidas: [
+      {
+        id: 3,
+        nome: 'Fritas com Ketchup',
+        imagem: require('../../../assets/Lanche4.jpg'),
+      },
+      {
+        id: 4,
+        nome: 'Fritas',
+        imagem: require('../../../assets/Fritas.jpg'),
+      },
+    ],
+  },
+  {
+    id: 3,
+    nome: 'Bebidas',
+    comidas: [
+      {
+        id: 5,
+        nome: 'Milkshake de ovomaltine',
+        imagem: require('../../../assets/Shake1.jpg'),
+      },
+      {
+        id: 6,
+        nome: 'Milkshake de Morango',
+        imagem: require('../../../assets/Shake2.jpg'),
+      },
+    ],
+  },
+];
 
 const Cardapio = () => {
+  const [modalVisible, setModalVisible] = useState(false);
+  const [selectedFood, setSelectedFood] = useState(null);
+
+  const toggleModal = (comida) => {
+    setSelectedFood(comida);
+    setModalVisible(!modalVisible);
+  };
+
   return (
     <ScrollView contentContainerStyle={styles.scrollViewContainer}>
       <View style={styles.container}>
-        {/* Imagem de capa (exemplo) */}
+     
         <Image source={require('../../../assets/Burger.jpeg')} style={styles.coverImage} />
 
-        {/* Título da seção */}
+      
         <Text style={styles.sectionTitle}>Categorias de Comida</Text>
         <Text style={styles.sectionTitle}>----------------------------</Text>
 
-        <Text style={styles.sectionTitle}>Burguers</Text>
-        {/* Lista de categorias (exemplo) */}
-        <View style={styles.categoryContainer}>
-
-          {/* Categoria 1 (exemplo) */}
-          <View style={styles.category}>
-            <Image source={require('../../../assets/Lanche1.jpeg')} style={styles.categoryImage} />
-            <Text style={styles.categoryTitle}>Combo fritas e refri</Text>
+        
+        {categorias.map((categoria) => (
+          <View key={categoria.id}>
+            <Text style={styles.sectionTitle}>{categoria.nome}</Text>
+            <View style={styles.categoryContainer}>
+              {/* Mapeando as comidas de cada categoria */}
+              {categoria.comidas.map((comida) => (
+                <View key={comida.id} style={styles.category}>
+                  <Image source={comida.imagem} style={styles.categoryImage} />
+                  <Text style={styles.categoryTitle}>{comida.nome}</Text>
+                  {/* Adicionando carrinho de compras */}
+                  <TouchableOpacity onPress={() => toggleModal(comida)} style={styles.cartButton}>
+                    <Image source={require('../../../assets/Carrinho.png')} style={styles.cartIcon} />
+                  </TouchableOpacity>
+                </View>
+              ))}
+            </View>
           </View>
+        ))}
 
-          {/* Categoria 2 (exemplo) */}
-          <View style={styles.category}>
-            <Image source={require('../../../assets/Lanche2.jpeg')} style={styles.categoryImage} />
-            <Text style={styles.categoryTitle}>Hambuerguer</Text>
+        {/* Modal */}
+        <Modal
+          animationType="slide"
+          transparent={true}
+          visible={modalVisible}
+          onRequestClose={() => {
+            setModalVisible(!modalVisible);
+          }}
+        >
+          <View style={styles.centeredView}>
+            <View style={styles.modalView}>
+              <Text style={styles.modalText}>Adicionado ao carrinho:</Text>
+              <Text style={styles.modalText}>{selectedFood?.nome}</Text>
+              <TouchableOpacity
+                style={{ ...styles.openButton, backgroundColor: '#2196F3' }}
+                onPress={() => setModalVisible(!modalVisible)}
+              >
+                <Text style={styles.textStyle}>Fechar</Text>
+              </TouchableOpacity>
+            </View>
           </View>
-        </View>
-
-        <Text style={styles.sectionTitle}>Fritas</Text>
-        {/* Lista de categorias (exemplo) */}
-        <View style={styles.categoryContainer}>
-
-          {/* Categoria 1 (exemplo) */}
-          <View style={styles.category}>
-            <Image source={require('../../../assets/Lanche4.jpg')} style={styles.categoryImage} />
-            <Text style={styles.categoryTitle}>Fritas com Ketchup</Text>
-          </View>
-
-          {/* Categoria 2 (exemplo) */}
-          <View style={styles.category}>
-            <Image source={require('../../../assets/Fritas.jpg')} style={styles.categoryImage} />
-            <Text style={styles.categoryTitle}>Fritas</Text>
-          </View>
-        </View>
-
-        <Text style={styles.sectionTitle}>Bebidas</Text>
-        {/* Lista de categorias (exemplo) */}
-        <View style={styles.categoryContainer}>
-
-          {/* Categoria 1 (exemplo) */}
-          <View style={styles.category}>
-            <Image source={require('../../../assets/Shake1.jpg')} style={styles.categoryImage} />
-            <Text style={styles.categoryTitle}>Milkshake de ovomaltine</Text>
-          </View>
-
-          {/* Categoria 2 (exemplo) */}
-          <View style={styles.category}>
-            <Image source={require('../../../assets/Shake2.jpg')} style={styles.categoryImage} />
-            <Text style={styles.categoryTitle}>Milkshake de Morango</Text>
-          </View>
-        </View>
+        </Modal>
       </View>
     </ScrollView>
   );
@@ -93,16 +145,18 @@ const styles = StyleSheet.create({
   categoryContainer: {
     flexDirection: 'row',
     justifyContent: 'space-between',
-    marginleft: '20px',
-  },
-  category: {
-    width: '48%',
-    marginBottom: 20,
-    borderRadius: 10,
-    overflow: 'hidden',
     marginLeft: 20,
     marginRight: 20,
+  },
+  category: {
+    width: '50%',
+    marginBottom: 20,
+    marginLeft: 10,
+    borderRadius: 10,
+    overflow: 'hidden',
     alignItems: 'center',
+    backgroundColor: '#1E90FF',
+    padding: 10,
   },
   categoryImage: {
     width: '100%',
@@ -113,6 +167,51 @@ const styles = StyleSheet.create({
     fontSize: 16,
     color: '#fff',
     fontWeight: 'bold',
+  },
+  cartButton: {
+    position: 'absolute',
+    top: 200,
+    right: 5,
+  },
+  cartIcon: {
+    width: 24,
+    height: 24,
+  },
+  centeredView: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 22,
+  },
+  modalView: {
+    margin: 20,
+    backgroundColor: 'white',
+    borderRadius: 20,
+    padding: 35,
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  openButton: {
+    backgroundColor: '#F194FF',
+    borderRadius: 20,
+    padding: 10,
+    elevation: 2,
+  },
+  textStyle: {
+    color: 'white',
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  modalText: {
+    marginBottom: 15,
+    textAlign: 'center',
   },
 });
 
